b89a1096a8bab789e18e5499fa0d1309cb2bd6af
MelinaRentzi <r.melina_92@hotmail.com>
2020-07-30 22:53:25 +0300

    crimdata link addition

--- 002-histogram/app.R
+++ 002-histogram/app.R
@@ -1,10 +1,4 @@
-####################################
-# Data Professor                   #
-# http://youtube.com/dataprofessor #
-# http://github.com/dataprofessor  #
-####################################
-
-# Modified from https://shiny.rstudio.com/tutorial/written-tutorial/lesson1/
+# https://stat.ethz.ch/R-manual/R-patched/library/datasets/html/crimtab.html
 
 library(shiny)
 data(crimtab)
